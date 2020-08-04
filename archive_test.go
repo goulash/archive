f934479857ef3ca96457455a1682fda3bda72196
Ben Morgan <neembi@gmail.com>
2020-08-04 21:45:45 +0200

    Add ExtractArchive and ExtractTar functions

--- archive_test.go
+++ archive_test.go
@@ -1,6 +1,7 @@
 package archive_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/goulash/archive"
@@ -10,6 +11,7 @@ func TestReadFileFromArchive(z *testing.T) {
 	const testdataPath = "testdata/dir_reader_data"
 	const archivePath = "dir1/file1"
 	const archiveData = "dir1/file1 content\n"
+
 	for _, ext := range []string{".tar", ".tar.gz", ".tar.bz2", ".tar.xz"} {
 		bs, err := archive.ReadFileFromArchive(testdataPath+ext, archivePath)
 		if err != nil {
@@ -18,5 +20,19 @@ func TestReadFileFromArchive(z *testing.T) {
 		if string(bs) != archiveData {
 			z.Errorf("expected data %q, got %q", archiveData, bs)
 		}
+	}
+}
+
+func TestExtractArchive(z *testing.T) {
+	const testdataArchive = "testdata/repoctl.tar.gz"
+	const destdir = "testdata/tmp"
+
+	defer func() {
+		os.RemoveAll(destdir)
+	}()
+
+	err := archive.ExtractArchive(testdataArchive, destdir)
+	if err != nil {
+		z.Errorf("unexpected error: %s", err)
 	}
 }
