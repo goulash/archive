1ccf41f11605a44bd204e2099a33899b920307e7
Ben Morgan <neembi@gmail.com>
2020-08-30 11:34:04 +0200

    Fix broken zst decompression

--- archive.go
+++ archive.go
@@ -226,8 +226,8 @@ func NewDecompressor(fpath string) (*Decompressor, error) {
 			return err
 		}
 		// Success:
-		d.file.Seek(0, 0)
 		zd.Reset(d.file)
+		d.file.Seek(0, 0)
 		d.reader = zd
 		d.closer = zd.IOReadCloser()
 		return nil
